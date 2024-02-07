fdbb7437605edb0c66d4e810681e179b2983ac09
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-02-07 14:41:49 +0300

    bonus task 2 done: deepEqual

--- lesson_3.js
+++ lesson_3.js
@@ -120,23 +120,24 @@ const obj1 = { here: { is: "on", other: "3" }, object: "Y" };
 const obj2 = { here: { is: "on", other: "2" }, object: "Y" };
 
 const deepEqual = (obj1, obj2) => {
-  if (obj1 === obj2) {
-    return true;
-  }
-  let keys1 = Object.keys(obj1);
-  let keys2 = Object.keys(obj2);
-  if (keys1 != keys2) {
+  if (obj1 === obj2) return true;
+
+  if (typeof obj1 !== "object" || obj1 === null ||
+      typeof obj2 !== "object" || obj2 === null   ) {
     return false;
   }
 
-  for (let key in obj1) {
-    if (!obj2.hasOwnProperty(key)) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+  
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
       return false;
     }
-    if(typeof obj1[key] === 'object' && typeof obj2[key] === 'object' ){
-
-    }
   }
+  return true;
 
 };
 
