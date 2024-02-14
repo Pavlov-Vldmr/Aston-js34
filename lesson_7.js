72357156e7f107cf58e5b3e0ef74b3e6bfb4c4d4
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-02-14 17:29:02 +0300

    task done Dom\Bom

---
+++ lesson_7.js
@@ -0,0 +1,50 @@
+/**
+ * 1. Почитать про принципы программирования KISS, DRY, YAGNI + почитать про "Антипаттерны" Чистого кода
+ * 
+ * KISS - Keep It Simple, Stupid / Будь проще
+ * Этот принцип гласит, что простые системы будут работать лучше и надежнее.
+ * 
+ * DRY - Don’t Repeat Yourself / Не повторяйтесь
+ * 
+ * YAGNI - You Aren’t Gonna Need It / Вам это не понадобится
+ * 
+ * Antipattern - https://habr.com/ru/companies/ruvds/articles/735910/
+ */
+
+/**
+ * 2. Прочитать про способы хранения `LocalStorage`, `SessionStorage` и `Cookie`
+ * 
+ * LocalStorage - https://developer.mozilla.org/ru/docs/Web/API/Window/localStorage
+ * SessionStorage - https://developer.mozilla.org/ru/docs/Web/API/Window/sessionStorage
+ * Cookie - https://developer.mozilla.org/ru/docs/Web/API/Document/cookie
+ */
+
+/**
+ * 3. HTML / CSS - Базовая структура html документа, БЭМ методология
+ * 
+ * HTML structure - https://www.w3schools.com/html/html_intro.asp
+ * БЭМ - https://ru.bem.info/methodology/
+ */
+
+/**
+ * 4. Почитать про паттерны функционального программирования + посмотреть примеры использования
+ * 
+ * https://habr.com/ru/articles/337880/
+ */
+
+/**
+ * 5. Способы позиционирования контента на странице
+ * 
+ * https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Positioning
+ */
+
+/**
+ * Веса селекторов
+ * 
+ * style=""         1,0,0,0
+ * #id              0,1,0,0
+ * .class           0,0,1,0
+ * [attr=value]     0,0,1,0
+ * Li               0,0,0,1
+ * *                0,0,0,0
+ */
